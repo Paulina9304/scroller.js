c3c0326d8cef6498aa98488dde189485d78da2f9
Paulina <paulinafijal2@gmail.com>
2020-01-29 18:44:55 +0100

    add basic styles and javaScript variables

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Hello world!')
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+
+    document.addEventListener('mousewheel', function(event) {
+        
+    })
+
+})
